fc0eae58c2a5e810079d7c011107c6787a46fd5d
Jeiferson de Almeida <dealmeida.jeiferson@gmail.com>
2021-01-19 22:33:51 -0300

    Adição das operações matemáticas e da lógica de funcionamento.

--- app/src/main/java/br/com/jeiferson/calculadorapdm20202/MainActivity.java
+++ app/src/main/java/br/com/jeiferson/calculadorapdm20202/MainActivity.java
@@ -7,21 +7,66 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvSelectedButton;
+    private TextView tvCalcContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvSelectedButton = findViewById(R.id.tvSelectedButton);
+        tvCalcContent = findViewById(R.id.tvCalcContent);
     }
 
-    public void onClick(View view) {
-        if (view instanceof Button) {
-            tvSelectedButton.setText(((Button) view).getText());
+    public void addNumber(View view) {
+        String concatenated = tvCalcContent.getText().toString() + ((Button) view).getText();
+        tvCalcContent.setText(concatenated);
+    }
+
+    public void addOperation(View view) {
+        String content = tvCalcContent.getText().toString();
+
+        System.out.println(content.matches("^.*[0-9]+$"));
+        if (content.length() == 0 || content.matches("^.*[0-9]+$") == false) {
+            return;
+        }
+
+        String operation = ((Button) view).getText().toString();
+        tvCalcContent.setText(tvCalcContent.getText() + operation);
+    }
+
+    public void addDot(View view) {
+        String content = tvCalcContent.getText().toString();
+
+        System.out.println(content.matches("^.*\\.[0-9]*$"));
+        if (content.matches("^.*\\.[0-9]*$")) {
+            return;
+        }
+
+        String operation = ((Button) view).getText().toString();
+        tvCalcContent.setText(tvCalcContent.getText() + operation);
+    }
+
+    public void clear(View view) {
+        tvCalcContent.setText("");
+    }
+
+    public void calc(View view) {
+        if (tvCalcContent.getText().toString().isEmpty()) {
+            return;
+        }
+
+        ScriptEngine sem = new ScriptEngineManager().getEngineByName("js");
+        try {
+            tvCalcContent.setText(sem.eval(tvCalcContent.getText().toString()).toString());
+        } catch (ScriptException se) {
+            se.printStackTrace();
         }
     }
+
 }
